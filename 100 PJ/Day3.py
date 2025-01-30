bd0b43a2dd09daad66a531145cdea0d44f86957e
May Thadar Kyaw <maythada.dku@gmail.com>
2025-01-30 14:13:05 +0530

    Python Practice

---
+++ 100 PJ/Day3.py
@@ -0,0 +1,16 @@
+#Day 3 PJ
+#Simple calculator
+name = input("What is your name?")
+input1 = int(input("Type First input: "))
+input2 = int(input("Type Second input: "))
+print(f"---Welcome {name}---")
+add = input1 + input2
+print(f"Addition is {add}.")
+sub = input1 - input2
+print(f"Subtraction is {sub}.")
+mult = input1 * input2
+print(f"Multiplication is {mult}.")
+div = input1 / input2
+print(f"Division is {div}.")
+mod = input1 % input2
+print(f"Modulus is {mod}.")
