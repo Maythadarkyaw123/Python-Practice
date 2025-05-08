ebe60e684e5146fd15c494a9c09bf25ea45e80d3
May Thadar Kyaw <maythada.dku@gmail.com>
2025-05-09 01:18:24 +0530

    Python Calculator

---
+++ calculator.py
@@ -0,0 +1,25 @@
+num1 = int(input("Enter first num: "))
+num2 = int(input("Enter second num: "))
+
+print("**********************************")
+
+add = num1 + num2
+print("Addition of ", num1, " and ", num2, " is ", add)
+
+sub = num1 - num2
+print("Subtraction of ", num1, " and ", num2, " is ", sub)
+
+multi = num1 * num2
+print("Multiplication of ", num1, " and ", num2, " is ", multi)
+
+div = num1 / num2
+print("Division of ", num1, " and ", num2, " is ", div)
+
+fdiv = num1 // num2
+print("Floor division of ", num1, " and ", num2, " is ", fdiv)
+
+modu = num1 % num2
+print("Modulus of ", num1, " and ", num2, " is ", modu)
+
+exp = num1 ** num2
+print("Exponent of ", num1, " and ", num2, " is ", exp)
